730d04b7613f0ed78489ea9a746f697b635e586c
Konstantin <VerisokoKT@yandex.ru>
2016-01-05 02:55:33 +0400

    OAuth token

---
+++ Indigo.Web/Providers/IndigoAuthorizationServerProvider.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Indigo.Web.Providers
+{
+    public class IndigoAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin");
+            if (allowedOrigin == null) allowedOrigin = "*";
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+            if (!context.UserName.Equals("demo") || !context.Password.Equals("demo"))
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return Task.FromResult<object>(null);
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+            identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+            identity.AddClaim(new Claim("sub", context.UserName));
+
+            var props = new AuthenticationProperties(new Dictionary<string, string> {
+                {"as:client_id", (context.ClientId == null) ? string.Empty : context.ClientId},
+                {"userName", context.UserName}
+            });
+
+            var ticket = new AuthenticationTicket(identity, props);
+            context.Validated(ticket);
+            return Task.FromResult<object>(null);
+        }
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
--- Indigo.Web/Startup.cs
+++ Indigo.Web/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using Indigo.Web.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
 using System.Collections.Generic;
@@ -14,9 +16,25 @@ namespace Indigo.Web
         public void Configuration(IAppBuilder app)
         {
             HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+
             WebApiConfig.Register(config);
             app.UseWebApi(config);
         }
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new IndigoAuthorizationServerProvider()
+            };
 
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
     }
 }
