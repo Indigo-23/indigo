1ade1f7337eac75076002c9556f39f8f86635086
Konstantin <VerisokoKT@yandex.ru>
2016-01-09 01:48:48 +0400

    signin & signup form

---
+++ Indigo.Web/_frontend_src/js/state/signup.js
@@ -0,0 +1,28 @@
+﻿indigo().state["signup"] = {
+    parent: 'layout',
+    url: '/signup',
+    views: {
+        'content@layout': {
+            templateUrl: 'signup/index.html',
+            controller: ['$scope', '$modal', function ($scope, $modal)
+            {
+                $scope.ShowLegal = function ()
+                {
+                    var modalInstance = $modal.open({
+                        animation: true,
+                        keyboard: true,
+                        templateUrl: 'legal/rules_use.html',
+                        size: 'lg',
+                        controller: ['$scope', function ($scope)
+                        {
+                            $scope.close = function ()
+                            {
+                                modalInstance.close();
+                            }
+                        }]
+                    });
+                }
+            }]
+        }
+    }
+};
